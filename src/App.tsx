f83f0f5ff86752dfb8cdcab5bc06e889a78ee2de
David <David@Davids-MacBook-Pro.local>
2024-03-26 21:06:56 +0800

    focus

--- src/App.tsx
+++ src/App.tsx
@@ -38,11 +38,11 @@ function App() {
       {/* Hero area */}
       <div className="my-10 md:my-20">
         <h1 className="text-slate-800 text-3xl md:text-5xl font-semibold mb-2">
-          Find the car wrecker in Perth for you
+          Find the car part you need
         </h1>
         <p className="text-slate-600 text-lg md:text-2xl font-regular">
-          Easy access to {carwreckers.length} car wreckers in Perth, Western
-          Australia
+          We'll contact {carwreckers.length} car wreckers across Perth to find
+          your part. 100% free.
         </p>
         <div className="flex flex-wrap mt-4 md:mt-8">
           <a href="https://tally.so/r/3xpJPr" target="_blank">
@@ -53,17 +53,15 @@ function App() {
               Find car parts
             </button>
           </a>
-          <a href="https://tally.so/r/npKD7q" target="_blank">
-            <button
-              className="bg-sky-500 hover:bg-sky-600 focus:outline-none focus:ring focus:ring-sky-300 active:bg-sky-700 px-6 md:px-8 py-2 mt-2 text-m md:text-xl rounded-full font-medium text-white"
-              onClick={sellYourCarEvent}
-            >
-              Sell your car
-            </button>
-          </a>
         </div>
       </div>
 
+      <h2>Prefer to contact Perth wreckers on your own?</h2>
+      <p>
+        {" "}
+        If you prefer to contact wreckers on your own, here's the contact
+        details for wreckers in Perth.
+      </p>
       {/* Carwrecker overview section */}
       <div className="grid sm: grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {carwreckers
