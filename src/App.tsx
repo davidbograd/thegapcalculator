056a0e1f8f99f365fa6ebe6ae46fad515e88d413
David <David@Davids-MacBook-Pro.local>
2024-04-27 10:28:31 +0800

    first commit of the gap calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,133 @@
-import CarwreckerCard from "./Components/CarwreckerCard";
-import SiteLogo from "./Components/Icons/SiteLogo.tsx";
-import carwreckers from "./carwreckerData";
+import { useState } from "react";
+import SiteLogo from "./Components/SiteLogo.tsx";
+import { LeaveCostCalc } from "/Users/David/Developer/Paternity leave/paternity-leave/src/Components/calculation";
 
 function App() {
+  const [salary, setSalary] = useState("100000");
+  const [totalLeave, setTotalLeave] = useState("40");
+  const [companyPaidLeave, setCompanyPaidLeave] = useState("12");
+  const [result, setResult] = useState(LeaveCostCalc(0, 0, 0, 0));
+
+  const handleButtonClick = () => {
+    console.log("I ran");
+    const salaryValue = parseFloat(salary);
+    const totalLeaveValue = parseFloat(totalLeave);
+    const companyPaidLeaveValue = parseFloat(companyPaidLeave);
+    setResult(
+      LeaveCostCalc(salaryValue, totalLeaveValue, companyPaidLeaveValue, 20)
+    );
+    function showResult() {
+      const element = document.getElementById("resultSection"); // Get the DIV element
+      element?.classList.remove("hidden"); // Remove mystyle class from DIV
+      element?.classList.add("block"); // Add newone class to DIV
+    }
+    showResult();
+  };
+
   return (
-    // Car wreckers section
+    //
     <div className="mx-auto max-w-7xl p-4 lg:pt-8">
-      <nav className="flex items-center justify-between">
+      <nav className="flex justify-center">
         <a href="#">
           <SiteLogo />
         </a>
-        <div className="">
-          <a
-            href="#"
-            className="text-md lg:text-lg font-semibold text-slate-700 hover:text-sky-500 mr-4 lg:mr-8"
-          >
-            Find parts
-          </a>
-          <a
-            href="#"
-            className="text-md lg:text-lg font-semibold text-slate-700 hover:text-sky-500"
-          >
-            Sell car
-          </a>
-        </div>
       </nav>
 
       {/* Hero area */}
       <div className="my-10 md:my-20">
-        <h1 className="text-slate-800 text-3xl md:text-5xl font-semibold mb-2">
-          Find the car part you need
+        <h1 className="text-slate-800 text-3xl md:text-5xl font-semibold mb-2 text-center">
+          Uncover the <span className="bg-purple-200 px-0.5">true cost</span> of
+          your parental leave
         </h1>
-        <p className="text-slate-600 text-lg md:text-2xl font-regular">
-          We'll contact {carwreckers.length} car wreckers across Perth to find
-          your part. 100% free.
+        <p className="text-slate-600 text-lg md:text-2xl font-regular text-center">
+          See why your you should xxx...
         </p>
-        <div className="flex flex-wrap mt-4 md:mt-8">
-          <a href="https://tally.so/r/3xpJPr" target="_blank">
-            <button className="bg-sky-500 hover:bg-sky-600 focus:outline-none focus:ring focus:ring-sky-300 active:bg-sky-700 px-6 md:px-8 py-2 mt-2 mr-2 text-m md:text-xl rounded-full font-medium text-white">
-              Find car parts
-            </button>
-          </a>
+        <div>
+          <form className="bg-white w-80 m-auto mt-8">
+            {/* input */}
+            <label className="block text-gray-700 text-sm font-bold mt-8 mb-1">
+              Annual salary
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="salary"
+              type="number"
+              required
+              value={salary}
+              onChange={(e) => setSalary(e.target.value)}
+            ></input>
+
+            {/* input */}
+            <label className="block text-gray-700 text-sm font-bold mt-8 mb-1">
+              Total leave (in weeks)
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="total-leave"
+              type="number"
+              value={totalLeave}
+              onChange={(e) => setTotalLeave(e.target.value)}
+            ></input>
+
+            {/* input */}
+            <label className="block text-gray-700 text-sm font-bold mt-8 mb-1">
+              Company paid leave
+            </label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="company-paid-leave"
+              type="number"
+              value={companyPaidLeave}
+              onChange={(e) => setCompanyPaidLeave(e.target.value)}
+            ></input>
+          </form>
+        </div>
+
+        <div className="flex justify-center">
+          <button
+            onClick={() => handleButtonClick()}
+            className="bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring focus:ring-indigo-300 active:bg-indigo-700 px-6 md:px-8 py-2 mt-2 mr-2 text-m md:text-xl rounded-full font-medium text-white"
+          >
+            Calculate true cost
+          </button>
+        </div>
+
+        {/* Results go here */}
+        <div className=" mt-16 hidden" id="resultSection">
+          <p className="text-slate-800 text-l md:text-xl px-0.5 font-semibold text-center">
+            The cost of your parental leave is
+          </p>
+          <p className="text-slate-800 text-3xl md:text-5xl font-semibold mb-2 text-center bg-purple-200 px-0.5">
+            ${result.summary.total}
+          </p>
+          <p className="text-slate-800 text-l md:text-xl px-0.5 font-semibold text-center">
+            in lost income.
+          </p>
+
+          <h2>Summary</h2>
+          <p className="text-slate-800 text-md md:text-lg px-0.5">
+            You’re taking {result.leaveDetail.totalLeave} weeks of parental
+            leave. {result.leaveDetail.companyPaidLeave} weeks paid by your
+            employer. {result.leaveDetail.govPaidLeave} weeks paid by the
+            government and {result.leaveDetail.unpaidLeave} weeks unpaid.
+          </p>
+
+          <h2 className="mt-8">Calculation details</h2>
+          <p className="text-slate-800 text-md md:text-lg px-0.5">
+            Lost income: ${result.summary.lostIncome}
+          </p>
+          <p className="text-slate-800 text-md md:text-lg px-0.5">
+            Lost super: ${result.summary.lostSuper}
+          </p>
+          <p className="text-slate-800 text-md md:text-lg px-0.5">
+            Gained income: ${result.summary.gainedIncome}
+          </p>
+          <p className="text-slate-800 text-md md:text-lg px-0.5">
+            Gained super: ${result.summary.gainedSuper}
+          </p>
         </div>
       </div>
-
-      <h2>Prefer to contact Perth wreckers on your own?</h2>
-      <p>
-        {" "}
-        If you prefer to contact wreckers on your own, here's the contact
-        details for wreckers in Perth.
-      </p>
-      {/* Carwrecker overview section */}
-      <div className="grid sm: grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {carwreckers
-          // Sort alphabetically
-          .sort((a, b) => (a.wreckername > b.wreckername ? 1 : -1))
-          .map((carwrecker, key) => {
-            return (
-              <CarwreckerCard
-                key={key}
-                wreckername={carwrecker.wreckername}
-                address={carwrecker.address}
-                email={carwrecker.email}
-                phone={carwrecker.phone}
-                website={carwrecker.website}
-                url={carwrecker.url}
-              />
-            );
-          })}
-      </div>
+      {}
     </div>
   );
 }
---
+++ src/Components/calculation.ts
@@ -0,0 +1,108 @@
+export function LeaveCostCalc(
+  salary: number,
+  totalLeaveWeeks: number,
+  companyPaidLeaveWeeks: number,
+  govPaidLeaveWeeks: number
+) {
+  // Format numbers
+  function numberFormatted(x: number) {
+    // Round numbers to 2 decimal points
+    function numberRounded(x: number) {
+      return x.toFixed(2);
+    }
+    // Add , every thousands
+    function numberWithCommas(x: any) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+
+    const roundedNumber = numberRounded(x);
+    const numberWithCommasResult = numberWithCommas(roundedNumber);
+    return numberWithCommasResult;
+  }
+
+  const LeaveDetails = {
+    salary: salary,
+    totalLeave: totalLeaveWeeks,
+    companyPaidLeave: companyPaidLeaveWeeks,
+    govPaidLeave: govPaidLeaveWeeks,
+    unpaidLeave: totalLeaveWeeks - companyPaidLeaveWeeks - govPaidLeaveWeeks,
+  };
+
+  const superannuationRate = 0.11;
+  const govWeekRate = 882.75;
+  const companyPaidWeekRate = LeaveDetails.salary / 52;
+
+  function IncomeAndLossCalculator(
+    weeks: number,
+    weekRate: number,
+    superRate: number
+  ) {
+    const income = weeks * weekRate;
+    const superannuation = income * superRate;
+    const total = income + superannuation;
+    return {
+      income: income,
+      superannuation: superannuation,
+      total: total,
+    };
+  }
+  let govPaidCalc = IncomeAndLossCalculator(
+    LeaveDetails.govPaidLeave,
+    govWeekRate,
+    0
+  );
+  let companyPaidCalc = IncomeAndLossCalculator(
+    LeaveDetails.companyPaidLeave,
+    companyPaidWeekRate,
+    superannuationRate
+  );
+  let lostPayCalc = IncomeAndLossCalculator(
+    LeaveDetails.totalLeave,
+    companyPaidWeekRate,
+    superannuationRate
+  );
+
+  let lostTotals = {
+    income: lostPayCalc.income,
+    super: lostPayCalc.superannuation,
+    total: lostPayCalc.total,
+  };
+
+  let incomeTotals = {
+    income: govPaidCalc.income + companyPaidCalc.income,
+    super: govPaidCalc.superannuation + companyPaidCalc.superannuation,
+    total: govPaidCalc.total + companyPaidCalc.total,
+  };
+
+  let summaryTotals = {
+    total: incomeTotals.total - lostTotals.total,
+    lostIncome: lostTotals.income,
+    lostSuper: lostTotals.super,
+    gainedIncome: incomeTotals.income,
+    gainedSuper: incomeTotals.super,
+  };
+
+  function formatObjectValues(obj: { [key: string]: any }): {
+    [key: string]: any;
+  } {
+    // Create a new object to store the updated values
+    let newObj: { [key: string]: any } = {};
+    // Iterate over each property in the object
+    for (let key in obj) {
+      // Check if the property value is a number
+      newObj[key] = numberFormatted(obj[key]);
+    }
+    // Return the new object with updated values
+    return newObj;
+  }
+
+  const LeaveCostResult = {
+    summary: formatObjectValues(summaryTotals),
+    leaveDetail: LeaveDetails,
+    incomeGov: formatObjectValues(govPaidCalc),
+    incomeCompany: formatObjectValues(companyPaidCalc),
+    lostTotals: formatObjectValues(lostTotals),
+  };
+  console.log(LeaveCostResult);
+  return LeaveCostResult;
+}
